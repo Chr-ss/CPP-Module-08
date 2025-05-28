f7354cb1e6f52c06a8ec70021672f1360940f3d2
Chr-ss <christian.rasche@bertelsmann.de>
2025-05-28 13:56:14 +0200

    work in progress ex00

---
+++ ex00/inc/easyfind.hpp
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   easyfind.hpp                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: christian.rasche <christian.rasche@stud      +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/11/09 14:58:12 by christian.r   #+#    #+#                 */
+/*   Updated: 2025/05/16 20:19:38 by christian.r   ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+# define RESET          "\033[0m"
+# define BLACK          "\033[30m"
+# define WHITE          "\033[37m"
+# define BRIGHT_WHITE   "\033[97m"
+# define RED            "\033[31m"
+# define BRIGHT_RED     "\033[91m"
+# define YELLOW         "\033[33m"
+# define BRIGHT_YELLOW  "\033[93m"
+# define BLUE           "\033[34m"
+# define LIGHT_BLUE     "\033[38;5;123m"
+# define BRIGHT_BLUE    "\033[94m"
+# define MAGENTA        "\033[35m"
+# define BRIGHT_MAGENTA "\033[95m"
+# define CYAN           "\033[36m"
+# define BRIGHT_CYAN    "\033[96m"
+# define GREEN          "\033[32m"
+# define BRIGHT_GREEN   "\033[92m"
+# define GRAY           "\033[90m"
+# define LIGHT_GRAY     "\033[37m"
+# define DARK_GRAY      "\033[90m"
+# define ORANGE         "\033[38;5;208m"
+# define PINK           "\033[38;5;213m"
+# define PURPLE         "\033[38;5;129m"
+# define BOLD           "\033[1m"
+# define UNDERLINE      "\033[4m"
+
+template <typename T>
+int easyfind(T &container, int value)
+{
+	typename T::iterator it = std::find(container.begin(), container.end(), value);
+	if (it == container.end())
+		throw std::runtime_error("Value not found in the container");
+	return *it;
+}
+
+
+#endif // EASYFIND_HPP
---
+++ ex00/src/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.cpp                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: crasche <crasche@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/11/07 16:36:52 by crasche       #+#    #+#                 */
+/*   Updated: 2025/05/15 22:26:54 by christian.r   ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/Whatever.hpp"
+
+int main( void ) 
+{
+	std::cout << BRIGHT_GREEN << BOLD << "\n\tSubject tests: \n" << RESET;
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
+}
